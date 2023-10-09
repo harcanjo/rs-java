8a0b1e7da6ad781d3d8520d0128cf7f0bc04b533
Hugo Arcanjo <hfarcanjo@gmail.com>
2023-10-09 11:16:11 -0300

    add user model and user controller

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# rs-java
-Rocketseat - Curso gratuito e online de Java
+# Rocketseat - Curso de Java
+
+## Projeto: To-do list
+Uma API Rest de "TODO's".
+
+## Requisitos
+- Spring Boot: version -> 3.0.11
+  - Dependencia: Spring Web
+- Maven
+- Java 17
+
---
+++ src/main/java/com/harcanjo/todolist/user/UserModel.java
@@ -0,0 +1,48 @@
+package com.harcanjo.todolist.user;
+
+public class UserModel {
+
+	private String username;
+	private String name;
+	private String password;
+	
+	public UserModel() {	
+	}
+
+	public UserModel(String username, String name, String password) {
+		this.username = username;
+		this.name = name;
+		this.password = password;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "UserModel [username=" + username + ", name=" + name + ", password=" + password + "]";
+	}
+	
+	
+}
