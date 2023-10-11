fdaefaad81cf5428dba0e5af7f92f4fd62977f40
Hugo Arcanjo <hfarcanjo@gmail.com>
2023-10-11 11:33:46 -0300

    add task repository

---
+++ src/main/java/com/harcanjo/todolist/task/ITaskRepository.java
@@ -0,0 +1,9 @@
+package com.harcanjo.todolist.task;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ITaskRepository extends JpaRepository<TaskModel, UUID>{
+
+}
