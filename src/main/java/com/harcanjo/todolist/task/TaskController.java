e8a7587f5ccf749bc644e835031321ab34cb5a17
Hugo Arcanjo <hfarcanjo@gmail.com>
2023-10-12 12:24:32 -0300

    add list tasks by user

--- src/main/java/com/harcanjo/todolist/task/TaskController.java
+++ src/main/java/com/harcanjo/todolist/task/TaskController.java
@@ -1,11 +1,13 @@
 package com.harcanjo.todolist.task;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,7 +23,7 @@ public class TaskController {
 	private ITaskRepository taskRepository;
 	
 	@PostMapping("/")
-	public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request) {		
+	public ResponseEntity<?> create(@RequestBody TaskModel taskModel, HttpServletRequest request) {		
 		var idUser = request.getAttribute("idUser");
 		taskModel.setIdUser((UUID)idUser);
 		
@@ -40,6 +42,13 @@ public class TaskController {
 		
 		var task = this.taskRepository.save(taskModel);
 		return ResponseEntity.status(HttpStatus.OK).body(task);
+	}
+	
+	@GetMapping("/")
+	public List<TaskModel> list(HttpServletRequest request) {
+		var idUser = request.getAttribute("idUser");
+		var tasks = this.taskRepository.findByIdUser((UUID)idUser);
+		return tasks;
 	}
 
 }
