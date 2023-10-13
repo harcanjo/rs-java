4e9e2881d493c317761bfd83c46f067afcba6239
Hugo Arcanjo <hfarcanjo@gmail.com>
2023-10-13 12:23:25 -0300

    add if task exist and task by user verification

--- src/main/java/com/harcanjo/todolist/task/TaskController.java
+++ src/main/java/com/harcanjo/todolist/task/TaskController.java
@@ -57,12 +57,24 @@ public class TaskController {
 	
 	//http://localhost:8080/tasks/5c269e75-819144bc959d-8849f535e401
 	@PutMapping("/{id}")
-	public TaskModel update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request) {
+	public ResponseEntity<?> update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request) {
 		var task = this.taskRepository.findById(id).orElse(null);
 		
-		Utils.copyNonNullProperties(taskModel, task);
+		if (task == null) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+					.body("Task not found.");
+		}
 		
-		return this.taskRepository.save(task);
+		var idUser = request.getAttribute("idUser");
+		
+		if (!task.getIdUser().equals(idUser)) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+					.body("User does not have permission to change this task.");
+		}
+		
+		Utils.copyNonNullProperties(taskModel, task);
+		var taskUpdated = this.taskRepository.save(task);
+		return ResponseEntity.ok().body(taskUpdated); 
 	}
 
 }
